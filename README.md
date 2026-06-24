0597e09c3a8f84b9e30c3483fe773eeb8c78dded
Patrick Adams <patrick_aust@hotmail.com>
2026-06-24 13:11:55 +0200

    commit before clean up

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # spider-silk-max4
+
+Analysis for spider silk experiment at max4
--- src/const.py
+++ src/const.py
@@ -3,7 +3,9 @@
 
 
 PROP_ID = 20251747
-DATA_PATH = f'/data/visitors/cosaxs/{PROP_ID}/2026052808'
+# DATA_PATH = f'/data/visitors/cosaxs/{PROP_ID}/2026052808'
+DATA_PATH = f'/home/pat/max4/spider-silk-max4/data'
+
 PHOTON_ENERGY = 12400.0
 DET_D = 2499.90 #Taken from SAXS_Z from metadata files. I think this refers to the Eiger distance?
 PILATUS_PX = 0.00017199999999999998
@@ -15,3 +17,5 @@ PILATUS_QRANGE = [1.25e-1, 2.125]
 
 EIGER_QBANDS = [ (0.0035,0.008), (0.009,0.06), (0.07,0.2) ]
 PILATUS_QBANDS = [ (0.15,0.25), (0.3,0.6), (0.75,1.1), (1.2,1.4), (1.6,1.9) ]
+
+
